238bf58be7771218a0790712426f75a954cbdfb8
itshenrique <its.henrique@hotmail.com>
2020-08-25 20:22:50 -0300

    adicionado pseudo-desktop-react

---
+++ projects/pdr/precache-manifest.e5f2e9d78cfb4ffc7dd5f22251adc971.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccd49980b98eadf78c70af57f077a9df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73483bec50ec61c03781",
+    "url": "/static/css/main.ef14790e.chunk.css"
+  },
+  {
+    "revision": "0202b835eef0e8c6c5c4",
+    "url": "/static/js/2.759fdff2.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.759fdff2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73483bec50ec61c03781",
+    "url": "/static/js/main.29a70e52.chunk.js"
+  },
+  {
+    "revision": "b242c22b475457e8510d",
+    "url": "/static/js/runtime-main.8ce7fb42.js"
+  }
+]);
